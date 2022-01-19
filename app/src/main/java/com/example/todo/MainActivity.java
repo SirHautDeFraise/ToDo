e3898e8332af8787eaf4cd07774c758c2a93b9bc
L.Thibault <laudethibault83@gmail.com>
2022-01-19 11:54:31 +0100

    Mise en place de l'affichage de la tache

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -7,13 +7,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView1=(TextView)findViewById(R.id.textView1);
+        Intent intent=new Intent(MainActivity.this,TodoCreateActivity.class);
+        startActivityForResult(intent, 2);// Activity is started with requestCode 2
+    }
+    // Call Back method  to get the Message form other Activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if(requestCode==2)
+        {
+            String message=data.getStringExtra("MESSAGE");
+            textView1.setText(message);
+        }
     }
 
     @Override
