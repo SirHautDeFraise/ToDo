a8ec0b536f4b91633e0d45b1f288a6cc669432e9
L.Thibault <laudethibault83@gmail.com>
2022-01-19 11:05:55 +0100

    Ajout du spinner

--- app/src/main/java/com/example/todo/TodoCreateActivity.java
+++ app/src/main/java/com/example/todo/TodoCreateActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TodoCreateActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,16 @@ public class TodoCreateActivity extends AppCompatActivity {
 
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
+
+        List<String> listSpin = new ArrayList<String>();
+        listSpin.add("Urgence basse");
+        listSpin.add("Urgence grande");
+
+        Spinner spinnerUrgency = (Spinner) findViewById(R.id.spinnerUrgency);
+
+        ArrayAdapter<String> spinner = new ArrayAdapter<String>( TodoCreateActivity.this, android.R.layout.simple_spinner_item, listSpin);
+        spinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerUrgency.setAdapter(spinner);
     }
 
     @Override
